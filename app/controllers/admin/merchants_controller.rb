33ad7c12e0ef344723e567ea5709f6ebb5b04148
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-07-27 15:27:55 -0400

    Update: updates MerchantsController for Admin to allow for linking

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -1,10 +1,10 @@
 class Admin::MerchantsController < ApplicationController
-
   def index
     @merchants = Merchant.all
   end
 
   def show
     @merchant = Merchant.find(params[:id])
+    @merchant_name = @merchant.name
   end
 end
