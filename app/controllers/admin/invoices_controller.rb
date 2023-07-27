cc86af96a1956d618bd5518ee08c6298ceda9c9a
Weston Schutt <wtschutt@gmail.com>
2023-07-27 10:49:22 -0600

    feature: Add controller action and view for Admin invoice show page - US 33

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -1,3 +1,13 @@
 class Admin::InvoicesController < ApplicationController
+<<<<<<< Updated upstream
 
+=======
+  def index
+    @invoices = Invoice.all
+  end
+
+  def show
+    @invoice = Invoice.find(params[:id])
+  end
+>>>>>>> Stashed changes
 end
