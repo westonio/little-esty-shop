32d8f243cc7faf4781eae1bd4f45ca75ef697133
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-07-28 13:29:19 -0400

    Refactor: moves show tests to show spec file

--- spec/features/merchant/merchant_invoices_index_spec.rb
+++ spec/features/merchant/merchant_invoices_index_spec.rb
@@ -3,13 +3,20 @@ require 'rails_helper'
 RSpec.describe 'Merchant Invoices Index' do
   before :each do
     @merchant1 = Merchant.create!(name: 'Hair Care')
+    @merchant2 = Merchant.create!(name: 'Jewelry')
+
     @customer1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
 
     @item1 = @merchant1.items.create!(name: 'Shampoo', description: 'This washes your hair', unit_price: 10,
                                       status: 0)
+    @item2 = @merchant2.items.create!(name: 'Conditioner', description: 'This makes your hair shiny', unit_price: 20,
+                                      status: 0)
+
     @invoice1 = @customer1.invoices.create!(status: 1)
+    @invoice2 = @customer1.invoices.create!(status: 1)
 
     InvoiceItem.create!(item: @item1, invoice: @invoice1, quantity: 1, unit_price: 10, status: 0)
+    InvoiceItem.create!(item: @item2, invoice: @invoice2, quantity: 1, unit_price: 20, status: 0)
   end
 
   # User Story 14 Testing Begins
@@ -30,58 +37,6 @@ RSpec.describe 'Merchant Invoices Index' do
     within "#invoice-#{@invoice1.id}" do
       expect(page).to have_link(@invoice1.id.to_s, href: merchant_invoice_path(@merchant1, @invoice1))
     end
-  end
-  # User Story 14 Testing End
-
-  # User Story 15 Testing Begins
-
-  # As a merchant
-  # When I visit my merchant's invoice show page (/merchants/:merchant_id/invoices/:invoice_id)
-  # Then I see information related to that invoice including:
-
-  # Invoice id
-  # Invoice status
-  # Invoice created_at date in the format "Monday, July 18, 2019"
-  # Customer first and last name
-
-  it 'displays the invoice id and status on the merchants show page' do
-    visit merchant_invoice_path(@merchant1, @invoice1)
-
-    expect(page).to have_content(@invoice1.id)
-    expect(page).to have_content(@invoice1.status)
-  end
-
-  it 'displays the invoice created_at date in the format "Monday, July 18, 2019"' do
-    visit merchant_invoice_path(@merchant1, @invoice1)
-
-    expect(page).to have_content(@invoice1.created_at.strftime('%A, %B %d, %Y'))
-  end
-
-  it 'displays the customer first and last name' do
-    visit merchant_invoice_path(@merchant1, @invoice1)
-
-    expect(page).to have_content(@customer1.first_name)
-    # User Story 15 Testing End
-  end
-
-  # User Story 16 Testing Begins
-
-  # As a merchant
-  # When I visit my merchant invoice show page (/merchants/:merchant_id/invoices/:invoice_id)
-  # Then I see all of my items on the invoice including:
-
-  # Item name
-  # The quantity of the item ordered
-  # The price the Item sold for
-  # The Invoice Item status
-  # And I do not see any information related to Items for other merchants
-
-  it 'displays item name, quantity, price, and invoice item status for this merchant only' do
-    visit merchant_invoice_path(@merchant1, @invoice1)
-
-    expect(page).to have_content(@item1.name)
-    expect(page).to have_content(@item1.quantity)
-    expect(page).to have_content(@item1.unit_price)
-    expect(page).to have_content(@item1.status)
+    # User Story 14 Testing End
   end
 end
