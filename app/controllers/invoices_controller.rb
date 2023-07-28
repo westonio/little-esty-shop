a8cbe1bdf5fb68e5eb066b5803e56615f371cb16
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-07-28 17:15:29 -0400

    Feat: adds update_status method

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -8,4 +8,12 @@ class InvoicesController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @invoice = Invoice.find(params[:id])
   end
+
+  def update_status
+    @invoice_item = InvoiceItem.find(params[:invoice_item_id])
+    @invoice_item.update!(status: params[:status])
+
+    redirect_to merchant_invoice_path(params[:merchant_id], @invoice_item.invoice_id),
+                notice: 'Status updated successfully'
+  end
 end
