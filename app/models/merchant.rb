6d0522a69159460ab13072fdb1c35fb0f908dbcc
Dani Wilson <danirae.powell@gmail.com>
2023-07-28 17:27:05 -0600

    Add favorite customers method
    Co-authored-by: Weston <wtschutt@gmail.com>

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -6,4 +6,9 @@ class Merchant < ApplicationRecord
   def invoices
     Invoice.joins(items: :merchant).where(items: { merchant: self }).distinct
   end
+
+  def favorite_customers
+    ids = invoices.pluck(:id)   
+    Customer.select("first_name, last_name, count(result)").joins(invoices: :transactions).where(transactions: {result: "success"}).where(invoices: {id: ids}).order(count: :desc).group("first_name, last_name").limit(5)
+  end
 end
