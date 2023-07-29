4c3bc2c11c04ee823f5dc4002a95d9815d2c3b75
Weston Schutt <wtschutt@gmail.com>
2023-07-29 10:19:03 -0600

    feature: Add Customer model method to query top 5 customers with most successful transactions

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,13 @@ class Customer < ApplicationRecord
   has_many :invoices
 
   validates_presence_of :first_name, :last_name
+
+  def self.top_five_with_most_success_transactions
+    Customer.select("first_name, last_name, count(result)")
+      .joins(invoices: :transactions)
+      .where(transactions: { result: "success" })
+      .group(:first_name, :last_name)
+      .order("count DESC")
+      .limit(5)
+  end
 end
