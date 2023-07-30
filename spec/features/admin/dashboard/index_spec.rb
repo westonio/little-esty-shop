6064057007eef160923f8818fe0cd6a9f3a4aad7
Dani Wilson <danirae.powell@gmail.com>
2023-07-30 14:51:45 -0600

    refactor: edit one tiny space

--- spec/features/admin/dashboard/index_spec.rb
+++ spec/features/admin/dashboard/index_spec.rb
@@ -117,6 +117,6 @@ RSpec.describe 'Admin Dashboard (index)', type: :feature do
         expect(page).to have_content("Cecelia Osinski - 2 purchases", count: 1)
         expect(page).to have_content("Leanne Kuhn - 2 purchases", count: 1)
       end
-   end
+    end
   end
 end
