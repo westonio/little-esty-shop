e5c8e22fd61538708c6048c9166e69de6d359c39
Anna Wiley <awiley33@gmail.com>
2023-07-30 20:10:20 -0500

    refactor: Add the capybara visit helper to the end of the before block to dry up the testing suite

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -8,11 +8,11 @@ RSpec.describe "Admin Merchants Index Page" do
     @merchant_4 = Merchant.create!(name: "Cummings-Thiel")
     @merchant_5 = Merchant.create!(name: "Williamson Group")
     @merchant_6 = Merchant.create!(name: "Bernhard-Johns")
+
+    visit admin_merchants_path
   end
 
   it "displays the name of each merchant in the system" do
-    visit admin_merchants_path
-
     expect(page).to have_content(@merchant_1.name)
     expect(page).to have_content(@merchant_2.name)
     expect(page).to have_content(@merchant_3.name)
@@ -23,8 +23,6 @@ RSpec.describe "Admin Merchants Index Page" do
   end
 
   it "links to the merchant show page when you click the merchant's name" do
-    visit admin_merchants_path
-
     click_link(@merchant_1.name)
     expect(current_path).to eq(admin_merchant_path(@merchant_1))
 
@@ -36,16 +34,14 @@ RSpec.describe "Admin Merchants Index Page" do
 
   describe "disable/enable merchants button" do
     it "disables or enables the merchant when clicked" do
-      visit admin_merchants_path
-
       expect(@merchant_1.status).to eq("disabled")
       click_button "Enable #{@merchant_1.name}"
+
       @merchant_1.reload
       expect(@merchant_1.status).to eq("enabled")
 
 
       visit admin_merchants_path
-
       expect(@merchant_2.status).to eq("disabled")
       click_button "Enable #{@merchant_2.name}"
 
@@ -54,7 +50,6 @@ RSpec.describe "Admin Merchants Index Page" do
     end
 
     it "displays the updated merchant status once clicked" do
-      visit admin_merchants_path
       click_button "Enable #{@merchant_1.name}"
       @merchant_1.reload
       
@@ -68,7 +63,6 @@ RSpec.describe "Admin Merchants Index Page" do
   end
 
   it "displays the enabled and disabled merchants in two separate sections" do
-    visit admin_merchants_path
     click_button "Enable #{@merchant_1.name}"
     click_button "Enable #{@merchant_2.name}"
     click_button "Enable #{@merchant_4.name}"
@@ -96,12 +90,10 @@ RSpec.describe "Admin Merchants Index Page" do
   end
 
   it "has a link to create a new merchant" do
-    visit admin_merchants_path
     expect(page).to have_link("Create New Merchant")
   end
 
   it "links to a form to add info and create new merchant" do
-    visit admin_merchants_path
     click_link("Create New Merchant")
 
     expect(current_path).to eq(new_admin_merchant_path)
@@ -111,7 +103,6 @@ RSpec.describe "Admin Merchants Index Page" do
 
   describe "new merchant form submission" do
     before :each do
-      visit admin_merchants_path
       click_link("Create New Merchant")
 
       fill_in "Name", with: "Omnia Vintage"
@@ -131,4 +122,24 @@ RSpec.describe "Admin Merchants Index Page" do
       end
     end
   end
+
+  xit "displays the top 5 merchants by total revenue generated" do
+
+  end
+
+  xit "links each top merchant name to its admin merchant show page" do
+
+  end
+
+#   As an admin,
+# When I visit the admin merchants index (/admin/merchants)
+# Then I see the names of the top 5 merchants by total revenue generated
+# And I see that each merchant name links to the admin merchant show page for that merchant
+# And I see the total revenue generated next to each merchant name
+
+# Notes on Revenue Calculation:
+
+# Only invoices with at least one successful transaction should count towards revenue
+# Revenue for an invoice should be calculated as the sum of the revenue of all invoice items
+# Revenue for an invoice item should be calculated as the invoice item unit price multiplied by the quantity (do not use the item unit price)
 end
