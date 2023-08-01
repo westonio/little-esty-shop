16442c2003151fa0d81b4ea75cd1c6f3bc38003b
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-08-01 18:13:43 -0400

    Feat: adds random_image method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,14 @@ module ApplicationHelper
   def logo_image
     Unsplash::Photo.find('c9FQyqIECds')
   end
+
+  def random_image
+    Unsplash::Photo.random
+  rescue StandardError => e
+    if e.message.include?("Rate Limit Exceeded")
+      nil
+    else
+      raise e
+    end
+  end
 end
