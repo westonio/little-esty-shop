114d29811c0dab2b6b9dfab0feb387fb593146e0
Anna Wiley <awiley33@gmail.com>
2023-08-01 20:05:14 -0500

    feat: Add functionality for user story 30

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -13,4 +13,25 @@ class Merchant < ApplicationRecord
     ids = invoices.pluck(:id)   
     Customer.select("first_name, last_name, count(result)").joins(invoices: :transactions).where(transactions: {result: "success"}).where(invoices: {id: ids}).order(count: :desc).group("first_name, last_name").limit(5)
   end
+
+  def self.top_merchants
+    Merchant.select('merchants.name, merchants.id, SUM(invoice_items.unit_price * invoice_items.quantity) AS total_revenue')
+    .joins(items: { invoice_items: { invoice: :transactions } })
+    .where(transactions: {result: "success"})
+    .group('merchants.id')
+    .order('total_revenue DESC')
+    .limit(5)
+  end
 end
+# When I visit the admin merchants index (/admin/merchants)
+# Then I see the names of the top 5 merchants by total revenue generated
+# And I see that each merchant name links to the admin merchant show page for that merchant
+# And I see the total revenue generated next to each merchant name
+
+# Notes on Revenue Calculation:
+
+# Only invoices with at least one successful transaction should count towards revenue
+# Revenue for an invoice should be calculated as the sum of the revenue of all invoice items
+# Revenue for an invoice item should be calculated as the invoice item unit price multiplied by the quantity (do not use the item unit price)
+
+# SELECT merchants.name, SUM(invoice_items.unit_price * invoice_items.quantity) AS total_revenue FROM "merchants" INNER JOIN "items" ON "items"."merchant_id" = "merchants"."id" INNER JOIN "invoice_items" ON "invoice_items"."item_id" = "items"."id" INNER JOIN "invoices" ON "invoices"."id" = "invoice_items"."invoice_id" INNER JOIN "transactions" ON "transactions"."invoice_id" = "invoices"."id" WHERE "transactions"."result" = 'success' GROUP BY "merchants"."id" ORDER BY total_revenue DESC LIMIT 5
