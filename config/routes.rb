fbc7dadd54fed3440cd92e904076780154b2fd73
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-08-02 12:11:34 -0400

    Feat: adds route for unsplash. Other changes are rubocop autoformatting and do not change any logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
+  get 'unsplash/get_photos'
 
   resources :merchants, only: :index do
     resources :dashboard, only: :index
-    resources :items, only: [:index, :show, :edit, :update, :new, :create]
-    resources :invoices, only: [:index, :show]
+    resources :items, only: %i[index show edit update new create]
+    resources :invoices, only: %i[index show]
     patch '/invoice_items/:invoice_item_id', to: 'invoices#update_status', as: 'invoice_item_update'
   end
 
   namespace :admin do
-    get "/", to: "dashboard#index"
-    resources :merchants, only: [:index, :show, :edit, :update, :new, :create]
-    resources :invoices, only: [:index, :show, :update]
+    get '/', to: 'dashboard#index'
+    resources :merchants, only: %i[index show edit update new create]
+    resources :invoices, only: %i[index show update]
   end
 end
