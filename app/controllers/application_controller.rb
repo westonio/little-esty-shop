98e3f75f3d8246ea7f9627ad64db2e958b8309a0
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-08-02 12:07:19 -0400

    Feat: adds logic to get photos from unsplash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,27 @@
 class ApplicationController < ActionController::Base
+  before_action :get_photos
 
-  def logo_image
-    photo = Unsplash::Photo.find("c9FQyqIECds")
+  def get_photos
+    response = HTTParty.get('https://api.unsplash.com/photos', headers: {
+                              'Authorization' => "Client-ID #{Unsplash::API_KEY}"
+                            })
+
+    if response.success?
+      @photos = JSON.parse(response.body)
+    else
+      @error_message = 'Error fetching photos from Unsplash.'
+    end
+  end
+
+  def get_random_photo
+    response = HTTParty.get('https://api.unsplash.com/photos/random', headers: {
+                              'Authorization' => "Client-ID #{Unsplash::API_KEY}"
+                            })
+
+    if response.success?
+      @photo = JSON.parse(response.body)
+    else
+      @error_message = 'Error fetching a random photo from Unsplash.'
+    end
   end
 end
